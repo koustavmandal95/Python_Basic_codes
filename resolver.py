75eb6833082f9ebded6e369b15cbf179ff825db4
koustavmandal95 <koustavmandal95@gmail.com>
2019-07-29 18:06:09 +0530

    added Kwargs retrival etc

--- resolver.py
+++ resolver.py
@@ -42,3 +42,12 @@ def _hypervolume(length,*lengths):
 	for item in lengths:
 		print(item)
 _hypervolume(3,5,7,9)
+''' How to acesss kwargs via dictionary '''
+def tag(name,**attributes):
+    result='<'+name
+    for key,value in attributes.items():
+        result+=' {k}="{v}"'.format(k=key,v=str(value))
+        result+='>'
+    return(result)
+a=tag("img",src="monet.jpg",alt="Sunrise by Claude Monet",border=1)
+print(a)
