f9048b46f8f7fe8e87c24ecbcaeff7670391d8e6
koustavmandal95 <koustavmandal95@gmail.com>
2019-08-06 17:35:35 +0530

    Update Replace_str.py

--- Replace_str.py
+++ Replace_str.py
@@ -9,5 +9,23 @@ def replace_str(s,a):
                 s[i:i+len(a)]='*'*len(a)
     k="".join(s)
     print(k)
+s=input()
+a=input()                                   ||
+def reverse_replace_str(s,a): #siva code ///  \\\\
+    s=list(s)
+    temp=a
+    a=list(a)
+    temp=list(temp)
+    k=[temp[i] for i in range(len(temp)-1,-1,-1)]
+    k="".join(k)
+    for i in range(0,len(s)):
+        if s[i-1]==" " or i==0:
+            if s[i:i+len(a)]==a:
+                s[i:i+len(a)]=k
+    k="".join(s)
+    print(k)
+
+reverse_replace_str(s,a)
+
 
 replace_str(s,a)
