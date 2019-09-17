8857b9a909095d816c22b4f67473313f997e0c4a
koustavmandal95 <koustavmandal95@gmail.com>
2019-09-17 22:31:56 +0530

    input, dictionary tricks

---
+++ Python_tricks.py
@@ -0,0 +1,18 @@
+print("{:,}".format(1234456677))
+#1,234,456,677
+a,b=(input()).split()
+print(type(a))
+arr=[]
+arr=list(map(int,input("Enter the list-> ").rstrip().split()))
+x, y, z = [int(x) for x in input("Enter three value: ").split()]
+print(x,y,z)
+
+
+from collections import OrderedDict
+def main_dict():
+    sportsTeams=[("Royals",(18,12)),("Rockets",(24,6)),("Cardinals",(20,10)),("Dragons",(22,8)),
+                 ("Kings",(15,15)),("Chargers",(20,10)),("Jets",(16,14)),("Warriors",(25,5))]
+    s=sorted(sportsTeams,key=lambda t:t[1][0],reverse=True)
+    teams=OrderedDict(s)
+    print(teams)
+main_dict()
